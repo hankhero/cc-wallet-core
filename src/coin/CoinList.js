21331b410d6778143ea2781fbffda28b62699458
Alex Mizrahi <alex.mizrahi@gmail.com>
2014-10-27 16:54:55 +0200

    getTotalValue refactoring (needs review!)

--- src/coin/CoinList.js
+++ src/coin/CoinList.js
@@ -38,40 +38,25 @@ CoinList.prototype.getCoins = function() {
 CoinList.prototype.getTotalValue = function(cb) {
   verify.function(cb)
 
-  var self = this
+  var self = this;
+  var dColorValues = {};
 
-  Q.fcall(function() {
-    var dColorValues = {}
-
-    function getMainColorValue(index) {
-      if (index === self.coins.length)
-        return dColorValues
-
-      return Q.ninvoke(self.coins[index], 'getMainColorValue')
-        .then(function(colorValue) {
-          var colorId = colorValue.getColorId()
-
-          if (_.isUndefined(dColorValues[colorId]))
-            dColorValues[colorId] = colorValue
-          else
-            dColorValues[colorId] = dColorValues[colorId].plus(colorValue)
-
-          return getMainColorValue(index + 1)
-        })
-    }
-
-    return getMainColorValue(0)
-
-  })
-  .then(function(dColorValues) {
-    var colorValues = Object.keys(dColorValues).map(function(colorId) {
-      return dColorValues[colorId]
-    })
-
-    return colorValues
-
-  })
-  .done(function(colorValues) { cb(null, colorValues) }, function(error) { cb(error) })
+  Q.all(self.coins.map(function (coin) {
+      return Q.ninvoke(coin, 'getMainColorValue').
+          then(function (colorValue) {
+               var colorId = colorValue.getColorId();
+               if (_.isUndefined(dColorValues[colorId]))
+                   dColorValues[colorId] = colorValue;
+               else
+                   dColorValues[colorId] = dColorValues[colorId].plus(colorValue);
+               return true;
+          });
+   })).then(function(all_ok) {
+       var colorValues = Object.keys(dColorValues).map(function(colorId) {
+           return dColorValues[colorId];
+       });
+       return colorValues;
+  }).done(function(colorValues) { cb(null, colorValues); }, function(error) { cb(error); });
 }
 
 
