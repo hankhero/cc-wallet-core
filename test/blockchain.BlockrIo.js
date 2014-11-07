2641416b55b78aca933e3d2eff53490879b1bbcf
Henrik Hjelte <henrik@henrikhjelte.com>
2014-11-07 14:55:00 +0100

    tidied up too much in blockchain tests

--- test/blockchain.BlockrIo.js
+++ test/blockchain.BlockrIo.js
@@ -1,5 +1,7 @@
 var blockchainImplementationTest = require('./blockchain.implementation.js')
 
+var blockchain = require('../src/blockchain')
+
 blockchainImplementationTest({
   // why describe.skip: BlockrIO is currently (6 nov 2014) not working,
   // we get frequent "Too many requests"
--- test/blockchain.Chain.js
+++ test/blockchain.Chain.js
@@ -1,5 +1,7 @@
 var blockchainImplementationTest = require('./blockchain.implementation.js')
 
+var blockchain = require('../src/blockchain')
+
 blockchainImplementationTest({
   name: 'blockchain.Chain',
   clazz: blockchain.Chain
